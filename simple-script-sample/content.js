fa9c09bfdba8c73a753689565d9e241b84f6da7f
kohari <kohari@example.com>
2019-12-20 11:44:10 +0900

    update

--- simple-script-sample/content.js
+++ simple-script-sample/content.js
@@ -1,1 +1,8 @@
-window.alert('[Sample]Alert Test');
+var div = document.createElement('div')
+div.textContent = Date()
+div.style.backgroundColor = "#ff0"
+div.style.position = "fixed"
+div.style.bottom = "10px"
+div.style.right = "10px"
+div.style.zIndex = 100000
+document.body.insertBefore(div, document.body.firstChild)
